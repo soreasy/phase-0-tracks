74f732f15e2f522bf1dfaea21f0d6987465c9ed7
Soren Zeliger <szeliger@oberlin.edu>
2016-06-15 22:49:16 -0400

    adds interview questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "We have lots of garlic bread. Want some?"
+garlic_desired = gets.chomp
+
+puts "Would you like to enroll in health insurance?"
+health_insurance_desired = gets.chomp
+
