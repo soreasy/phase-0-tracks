1834328fb476d03b775aa268452e344c906643c2
Soren Zeliger <szeliger@oberlin.edu>
2016-07-03 22:20:19 -0400

    adds pseudocode for puppy species to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# Release 0
+
+# SPECIES ---
+# Puppy
+
+# CHARACTERISTICS ---
+# Fur color: varies
+# Eye color: varies
+# Legs: 4
+# Bark volume: varies b/w 1 and 10
+# Personality_type: varies
+
+# BEHAVIOR ---
+# Jump
+# Wag Tail
+# Be Cute
+# Run
