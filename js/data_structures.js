0d422dc6240646c70d89a7352eea780480359bc6
Soren Zeliger <szeliger@oberlin.edu>
2016-07-16 18:19:51 -0400

    Initial commit - initializes name and colors arrays

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "yellow", "green"];
+var names = ["Ed", "Wanda", "Tyrone", "Macy"];
+
+colors.push("magenta");
+names.push("Jess");
+
+console.log(colors);
+console.log(names);
