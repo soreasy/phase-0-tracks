91d85d1c7f580ffa60ffb2fa80872af5ee12222a
Soren Zeliger <szeliger@oberlin.edu>
2016-07-16 18:47:19 -0400

    Creates car function

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,28 @@ var names = ["Ed", "Wanda", "Tyrone", "Macy"];
 colors.push("magenta");
 names.push("Jess");
 
-console.log(colors);
-console.log(names);
+
+var horsey_names_colors_object = {};
+
+for (var i = 0; i < colors.length; i++) {
+	horsey_names_colors_object[names[i]] = colors[i];
+}
+
+console.log(horsey_names_colors_object);
+
+function Car(name, age, isBroken) {
+	this.name = name;
+	this.age = age;
+	this.isBroken = isBroken;
+
+	this.turnOn = function() { console.log("The engine is on!"); };
+
+}
+
+var newCar = Car.new("Soren's car", 10, false)
+
+for (var key in newCar) {
+	if (newCar.hasOwnProperty(key)) {
+		console.log(key + " -> " + newCar[key]);
+	}
+}
